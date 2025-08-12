f1115a78887248a50ac3120db62b1c34595c5d90
Ivan Sanchez <dr1179019@gmail.com>
2025-08-12 02:30:59 -0500

    feat: Add environment configuration for development and update product service to use environment variables

--- src/app/shared/services/productService.ts
+++ src/app/shared/services/productService.ts
@@ -2,17 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product } from '../models/product';
 import { catchError, map, Observable, pipe, shareReplay, throwError } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
-  private readonly url: string = 'http://localhost:8081/sistema/api/v1/productos';
   private readonly http = inject(HttpClient);
   
   public Products(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.url).pipe(
+    return this.http.get<Product[]>(environment.api).pipe(
       shareReplay(1) // guarda la última respuesta y la comparte
       , catchError(err => {
         return throwError(() => new Error('Error al obtener productos'));
@@ -21,7 +20,7 @@ export class ProductService {
   }
 
   public CreateProduct(product:Product){
-    return this.http.post<Product>(this.url, product, {}).pipe(
+    return this.http.post<Product>(environment.api, product, {}).pipe(
       catchError(err => {
         console.error(err);
         return throwError(() => new Error('Error al crear el producto'));
@@ -30,7 +29,7 @@ export class ProductService {
   }
 
   public UpdateProduct(product:Product){
-    return this.http.put(this.url, product).pipe(
+    return this.http.put(environment.api, product).pipe(
       catchError(err => {
         console.error(err);
         return throwError(() => new Error('Error al actualizar el producto'));
@@ -39,7 +38,7 @@ export class ProductService {
   }
 
   public DeleteProduct(product:Product){
-    return this.http.delete(this.url + '/'+ product.id).pipe(
+    return this.http.delete(environment.api + '/'+ product.id).pipe(
       catchError(err => {
         console.error(err);
         return throwError(() => new Error('Error al eliminar el producto'));
